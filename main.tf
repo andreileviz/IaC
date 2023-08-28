e40a9ae8d65f2a9113791af33db36a65eca62f49
Andrei Levi <126117114+andreileviz@users.noreply.github.com>
2023-08-28 17:31:29 -0300

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,25 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-} 
-
-provider "aws" {
-  profile = "default"
-  region = "us-west-2"
+module "aws-dev" {
+  source = "../../infra"
+  instancia = "t2.micro"
+  regiao_aws = "us-west-2"
+  chave = "IaC-DEV"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-03f65b8614a860c29"
-  instance_type = "t2.micro"
-  key_name = "IaC-Alura"
-
- tags = {
-    Name = "Primeira instância"
-  }
+output "IP" {
+  value = module.aws-dev.IP_publico
 }
